e3f147765088519f51e286ca72c1a60d4bb6d18f
juary-junior <128549562+juary-junior@users.noreply.github.com>
2024-05-28 13:43:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Trabalho_faculdade_1
+ Trabalho q consiste em ensinar aos usuários assuntos pertinente a máteria de lógica de computação.
---
+++ pages/scripts/script3.js
@@ -0,0 +1,17 @@
+const botaoFeedback = document.getElementById("botao-feedback");
+
+botaoFeedback.addEventListener("click", function(e){
+    e.preventDefault();
+    window.location.href = 'index.html';
+});
+
+function showSummary(type) {
+    const container = document.getElementById("container");
+    const feedback = document.getElementById("feedback");
+    if (type === 'certa') {
+        window.location.href = 'page4.html';
+    } else {
+        container.classList.add("hidden")
+        feedback.classList.remove("hidden")
+    }
+}
